29a74708cfd7381e39433d8f54e208ade99830ca
Edmund Yu <eyu1@ualberta.ca>
2021-09-28 02:59:21 +0000

    Implemented generateRandomString function to generate a random alphanumeric 6 character string and Post endpoint.

--- express_server.js
+++ express_server.js
@@ -1,20 +1,51 @@
 const express = require("express");
 const app = express();
+var morgan = require("morgan");
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+//returns a string of 6 random alphanumeric characters:
+function generateRandomString() {
+  let arr = [];
+  let randomNum;
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let i = 0;
+  while (i <= 6) {
+    let randomNum = Math.floor(Math.random() * characters.length);
+    let nextLetter = characters.charAt(randomNum);
+    arr.push(nextLetter);
+    if (arr.length > 6) {
+      return arr.join("");
+    }
+    i++;
+  }
+}
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -23,12 +54,7 @@ app.get("/urls/:shortURL", (req, res) => {
     shortURL,
     longURL: urlDatabase[shortURL],
   };
-
   res.render("urls_show", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
 });
 
 app.get("/urls.json", (req, res) => {
